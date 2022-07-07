4e0246acd21cd04c8500116c094b26e10e8409ce
Jaye <jshin0219@gmail.com>
2022-07-07 14:56:39 +0900

    [수정]이미지 저장 파일 포맷

--- hea17h-server/src/utils/s3.js
+++ hea17h-server/src/utils/s3.js
@@ -28,8 +28,7 @@ const upload = multer({
       acl: 'public-read', // 클라이언트에서 자유롭게 가용하기 위함
       key: (req, file, cb) => {
           let extension = path.extname(file.originalname);
-          cb(null, 'test/'+Date.now().toString()+extension);
-          // cb(null, file.originalname)
+          cb(null, file.originalname)
       },
   }),
   limits: { fileSize: 5 * 1024 * 1024 }, // 용량 제한
