a27a0145588ecce96c11f08cebed74025c71de7f
choheeseung <gmltmd0816@naver.com>
2022-07-10 08:18:20 +0900

    [추가] 로그인, 회원가입 추가

--- hea17h-client/src/App.js
+++ hea17h-client/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
-import CoachingWrite from './components/expert/CoachingWrite';
-import Nav from './components/common/Nav';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import Signup from './pages/Signup/Signup';
+import SignupComplete from './pages/Signup/SignupComplete';
+import SignupDetail from './pages/Signup/SignupDetail';
 
 function App() {
     return (
         <div className="App">
-            <Nav />
-            <CoachingWrite />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/signup/complete" element={<SignupComplete />} />
+                    <Route path="/signup/detail" element={<SignupDetail />} />
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
---
+++ hea17h-client/src/pages/Login/Login.jsx
@@ -0,0 +1,157 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Login() {
+    return (
+        <Container>
+            <LoginContainer>
+                <h1>로그인</h1>
+                <InputForm>
+                    <InputText>아이디</InputText>
+                    <InputItem />
+                </InputForm>
+                <InputForm>
+                    <InputText>비밀번호</InputText>
+                    <InputItem />
+                </InputForm>
+                <SocialLoginButton>
+                    <GoogleButton>
+                        Google 계정으로 로그인
+                    </GoogleButton>
+                    <KakaoButton>
+                        카카오 계정으로 로그인
+                    </KakaoButton>
+                    <NaverButton>
+                        네이버 계정으로 로그인
+                    </NaverButton>
+                </SocialLoginButton>
+                    <LoginButton>
+                        로그인
+                    </LoginButton>
+                    <CreateIdButton>
+                        계정 생성하기
+                    </CreateIdButton>
+            </LoginContainer>
+        </Container>
+    );
+}
+
+const Container = styled.div`
+    position: absolute;
+    display: flex;
+    margin: 0 auto;
+    padding: 0;
+    background-color: #faf3e3;
+    
+    width: 100%;
+    height: 100%;
+`;
+
+const LoginContainer = styled.div`
+    margin: auto;
+    background-color: white;
+    width: 500px;
+    height: 600px;
+
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    box-shadow: 10px 5px 5px lightgray;
+`;
+
+const InputForm = styled.form`
+    margin: 0 auto;
+    width: 400px;
+    height: 50px;
+
+    font-size: medium;
+    display: block;
+`;
+
+const InputText = styled.h4`
+    margin-left: 50px;
+    text-align: left;
+
+    margin-bottom: 5px;
+`;
+
+const InputItem = styled.input`
+    width: 300px;
+    height: 30px;
+`;
+
+const SocialLoginButton = styled.div`
+    margin: 50px auto ;
+`;
+
+const GoogleButton = styled.button`
+    margin: 5px 0;
+    width: 310px;
+    height: 40px;
+
+    background-color: white;
+    border-color: gray;
+
+    font-weight: 600;
+    font-size: 14px;
+
+    border-radius: 5px;
+`;
+
+const KakaoButton = styled.button`
+    margin: 5px 0;
+    width: 310px;
+    height: 40px;
+
+    background-color: #F7E600;
+    border: 1px solid transparent;
+
+    font-weight: 600;
+    font-size: 14px;
+    
+    border-radius: 5px;
+`;
+
+const NaverButton = styled.button`
+    margin: 5px 0;
+    width: 310px;
+    height: 40px;
+
+    background-color: white;
+    border: 1px solid #2DB400;
+    color: #2DB400;
+
+    font-weight: 600;
+    font-size: 14px;
+    
+    border-radius: 5px;
+`;
+
+const LoginButton = styled.button`
+    width: 130px;
+    height: 50px;
+    margin: 10px 180px;
+
+    color: white;
+    background-color: #51cf66;
+    border: 1px solid transparent;
+    font-size: medium;
+    font-weight: 800;
+
+    border-radius: 5px;
+`;
+
+const CreateIdButton = styled.button`
+    border: none;
+    background-color: inherit;
+    padding: 14px 28px;
+    font-size: 16px;
+    cursor: pointer;
+    display: inline-block;
+
+    text-decoration: underline;
+    color: #999999;
+`;
+
+export default Login;
