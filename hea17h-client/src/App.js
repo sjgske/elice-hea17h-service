7f4e13550e3560ba5ee8708a5ddc0b48642f0573
YONGHWAN <younghwan101@gmail.com>
2022-07-11 08:41:25 +0900

    [수정] diet.jsx 파일 수정, 파일 구조 정리

--- hea17h-client/src/App.js
+++ hea17h-client/src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Diet from './pages/';
 
 function App() {
-    return <div className="App">hello</div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/signup/complete" element={<SignupComplete />} />
+                    <Route path="/signup/detail" element={<SignupDetail />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
