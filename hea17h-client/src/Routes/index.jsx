07b11951511574c1a9b68099629404239ad82795
heejung <cheej10@naver.com>
2022-07-11 16:37:20 +0900

    [추가] Home 페이지

--- hea17h-client/src/Routes/index.jsx
+++ hea17h-client/src/Routes/index.jsx
@@ -2,11 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import Coaching from '../Pages/expert/Coaching';
 import CoachingWrite from '../Pages/expert/CoachingWrite';
 import CoachingRead from '../Pages/expert/CoachingRead';
+import Home from '../Pages/Home/Home';
 
 function Router() {
     return (
         <Routes>
-            <Route path="/" />
+            <Route path="/" element={<Home />} />
             <Route path="/coaching" element={<Coaching />} />
             <Route path="/coachingWrite" element={<CoachingWrite />} />
             <Route path="/coachingRead" element={<CoachingRead />} />
