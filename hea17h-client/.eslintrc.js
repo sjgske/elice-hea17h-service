fc55f7e6bd93bc9c5e09e7ec82720043af64af86
Danheon <eksgjs3847@gmail.com>
2022-07-18 23:11:25 +0900

    [추가] 유저 식단 목록 api 연결

--- hea17h-client/.eslintrc.js
+++ hea17h-client/.eslintrc.js
@@ -27,5 +27,6 @@ module.exports = {
         'space-unary-ops': 2,
         'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
         'react/prop-types': 'off',
+        'no-underscore-dangle': 'off',
     },
 };
